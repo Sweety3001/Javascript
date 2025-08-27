effa11e3692e70748aa906ba343d5bb0dfa2de78
Sweety3001 <sweetylog19@gmail.com>
2025-08-27 03:56:10 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all code as newer version
+
+// alert(3 + 3) ...// we are usinh nodejs, not browser
+
+let name="sweety"
+let age=15
+let isLoggedin = false
+
+// number, bigint, string, boolean , null, undefined => these are the datatypes in js
+
+// null=> standalone value, empty value
+// undefined=> value not assigned till now
+//object 
+console.log(typeof "sweety")
+console.log(typeof null) //object
