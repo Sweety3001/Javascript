5c598150ee649a13932c7dc9814e0d8bfe593de6
Sweety3001 <sweetylog19@gmail.com>
2025-08-27 04:13:26 +0000

    convesrion of datatypes

---
+++ conversion.js
@@ -0,0 +1,12 @@
+let score= undefined
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+ // for 33ac =  NaN, null = 0, true=1, false =0, " "= false
+
+ let num=22
+ let stringNum= String(num)
+ console.log(typeof stringNum);
+ 
