d3b2576f976ba57feb237213e9ff45e17735757d
Sweety3001 <sweetylog19@gmail.com>
2025-08-27 04:24:50 +0000

    operation basic

---
+++ operations.js
@@ -0,0 +1,24 @@
+let value=3
+let negvalue= -value
+console.log(negvalue);
+
+// +, -, *, **, /, %
+let str1="hello"
+let str2=" sweety"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log(1+2 + "2");  //32
+console.log("1"+2 + 2); //122
+
+console.log(+true); //1
+console.log(+ ""); //0
+
+let num1, num2, num3
+num1=num2=num3= 2+2
+
+let gameCOunter= 100
+gameCOunter++
+console.log(gameCOunter);
