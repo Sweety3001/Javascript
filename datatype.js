71baa76bf2b74ac5510cdd4db657090245ec7795
Sweety3001 <sweetylog19@gmail.com>
2025-09-04 03:41:55 +0000

    comparison

--- datatype.js
+++ datatype.js
@@ -13,3 +13,29 @@ let isLoggedin = false
 //object 
 console.log(typeof "sweety")
 console.log(typeof null) //object
+
+
+/*Primitive datatype 
+7 types: string, Number, Boolean, null, undefined, Symbol, BigInt
+*/
+
+const score = 100
+const scoreValue = 100.3
+const isLogged = false
+const outsideTemp = null
+let userEmail 
+
+const id= Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId );
+
+/* Reference(Non- Primitive) 
+Array, Objects, Functions
+*/
+
+
+const heros = ["sk", "nj" , "dg"];
+let myObj = {
+    name: "sweett",
+    ag: 22,
+}
