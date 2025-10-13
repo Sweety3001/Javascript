befa235177ade0cfe8e2d692252e5f7cf45f34ae
Sweety3001 <sweetylog19@gmail.com>
2025-10-13 07:39:20 +0000

    iterators

---
+++ iterators/dowhile.js
@@ -0,0 +1,14 @@
+
+let i =0
+while (i<=10) {
+    console.log(`Value of index is ${i}`);
+    i=i+2
+}
+
+let myArray = ['flash', 'das', 'trsdf']
+
+let score =1
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while (score <= 10);
